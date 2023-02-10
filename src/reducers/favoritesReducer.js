62fcc675fbb97c63265c7cc013470ae179fca01a
Joshua Kerr <kerrjoshua@gmail.com>
2023-02-10 16:16:57 +0900

    combineReducers implemented.

---
+++ src/reducers/favoritesReducer.js
@@ -0,0 +1,20 @@
+
+
+
+const initialState = {
+    favorites: [],
+    displayFavorites: false
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        // case ADD_FAVORITE:
+        //     return { ...state,
+                
+        //     }
+        default:
+            return state;
+    }
+}
+
+export default reducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
 
 import movieReducer from './movieReducer';
+import favoritesReducer from './favoritesReducer';
 
-export default movieReducer;
+const rootReducer = combineReducers({movie: movieReducer, favorites: favoritesReducer})
+
+export default rootReducer;
